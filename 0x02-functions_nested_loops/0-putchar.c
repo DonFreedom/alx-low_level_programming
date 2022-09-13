554081b442e9315bf6b995c568a6bcdf22c1c371
donfreedom <freedomchukwuka01@gmail.com>
2022-09-13 10:16:03 -0700

    putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** main - prints putchar
+* Return: always 0
+*/
+int main(void)
+{
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchat("\n");
+
+	return (0);
+}
