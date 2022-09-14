b1e5a5e3d09501cd83285f79325f6469d97b5b7d
donfreedom <freedomchukwuka01@gmail.com>
2022-09-14 00:48:13 -0700

    fixed putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchat("\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchat('\n');
 	return (0);
 }
